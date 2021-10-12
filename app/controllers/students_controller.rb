d5d073bd0cd97ffab12596479b6300f0f9f81034
MZidar <MZidar@gmail.com>
2021-10-12 14:34:24 -0400

    commit tree cdec51a6d50331ba67176681d4f261c6483c18a5

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,26 @@
 class StudentsController < ApplicationController
 
   def index
+    if name = params[:name]
+      students = Student.where(first_name: name.capitalize).or(Student.where(last_name: name.capitalize))
+      render json: students
+
+    
+    else  
     students = Student.all
     render json: students
+
+    end
+   
+    
+  
+  
+     
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 puts '🌱 Seeding students...'
 
-Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 99)
-Student.create(first_name: 'Idris', last_name: 'Elba', grade: 105)
-Student.create(first_name: 'Vanessa', last_name: 'Kirby', grade: 85)
-Student.create(first_name: 'Jason', last_name: 'Statham', grade: 70)
+# Student.create(first_name: 'Dwayne', last_name: 'Johnson', grade: 99)
+# Student.create(first_name: 'Idris', last_name: 'Elba', grade: 105)
+# Student.create(first_name: 'Vanessa', last_name: 'Kirby', grade: 85)
+# Student.create(first_name: 'Jason', last_name: 'Statham', grade: 70)
+Student.create(first_name: 'Kirby', last_name: 'Bochaio', grade: 95)
+
 
 puts '✅ Done seeding'
